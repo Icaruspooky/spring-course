1f9e47e5d3e5a5e9759ac48be3928568bdadc2af
Ícaro Cristofolini <icaro.cristofolini@projuris.com.br>
2019-07-22 21:40:58 -0300

    Inclusão de auto wiring dinâmico

--- src/main/java/com/curso/spring/springin10steps/BubbleSortAlgorithm.java
+++ src/main/java/com/curso/spring/springin10steps/BubbleSortAlgorithm.java
@@ -1,8 +1,10 @@
 package com.curso.spring.springin10steps;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class BubbleSortAlgorithm implements SortAlgorithm{
 
 	public int[] sort(int[] numbers) {
--- src/main/java/com/curso/spring/springin10steps/QuickSortAlgorithm.java
+++ src/main/java/com/curso/spring/springin10steps/QuickSortAlgorithm.java
@@ -1,5 +1,8 @@
 package com.curso.spring.springin10steps;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class QuickSortAlgorithm implements SortAlgorithm {
 
 	public int[] sort(int[] numbers) {
